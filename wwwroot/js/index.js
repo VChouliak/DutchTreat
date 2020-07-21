092c01af49009912301bbddaa77ea0db8867e8b2
VChouliak <chouliak@hotmail.com>
2020-07-21 15:52:48 +0200

    Implement basic content to check functionality

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,1 +1,22 @@
-﻿
+﻿$(document).ready(() => {
+    const messageForm = $("#messageForm");
+    const buyButton = $("#buyButton");
+    const productInfo = $(".product-props li");
+    const loginToggle = $("#loginToggle");
+    const popupForm = $(".popup-form");
+
+    messageForm.hide();
+
+    buyButton.on("click", () => {
+        alert("Is buied!!!");
+    });
+
+    productInfo.on("click", () => {
+        console.log("you clicked on " + $(this).text());
+    });
+
+    loginToggle.on("click", function () {
+        popupForm.toggle(1000);
+    });
+}
+);
