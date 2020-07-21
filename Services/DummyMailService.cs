87ed2232eb814087816286b531d17c44ca0a0d1b
VChouliak <chouliak@hotmail.com>
2020-07-21 19:55:20 +0200

    Create IMailService and implement as DummyMailService

---
+++ Services/DummyMailService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace DutchTreat.Services
+{
+    public class DummyMailService : IMailService
+    {
+        private ILogger<DummyMailService> _logger;
+
+        public DummyMailService(ILogger<DummyMailService> logger)
+        {
+            this._logger = logger;
+        }
+        public void SendMessage(string to, string subject, string body)
+        {
+            _logger.LogInformation($"To: {to}\nSubject: {subject}\nBody: {body}\n");
+        }
+    }
+}
