28debf7082faebd1301355f340b808dae85aed6e
VChouliak <chouliak@hotmail.com>
2020-07-21 17:31:02 +0200

    Create menu

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -8,16 +8,20 @@ namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+        
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet("contact")]
         public IActionResult Contact()
         {
             ViewBag.Title = "Contact us";
             return View();
         }
+
+        [HttpGet("about")]
         public IActionResult About()
         {
             ViewBag.Title = "About us";
