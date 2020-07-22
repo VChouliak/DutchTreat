5fc6795a4fd4f315ff2e7795b01cf21aa3987801
VChouliak <chouliak@hotmail.com>
2020-07-22 16:07:01 +0200

    Create DutchTreatDbContext

---
+++ Data/DutchTreatContext.cs
@@ -0,0 +1,15 @@
+﻿using DutchTreat.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Data
+{
+    public class DutchTreatContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
