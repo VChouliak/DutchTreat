5ed6fcd1b374a19af61b2d1549c0811b3730febf
VChouliak <chouliak@hotmail.com>
2020-07-23 11:45:31 +0200

    Create and implement CRUDRepository

---
+++ Data/Repositories/ICRUDRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Data.Repositories
+{
+    public interface ICRUDRepository<T>
+    {
+        public bool AddEntity(T entity);
+        public IEnumerable<T> GetAll();
+        public bool DeleteEntity(T entity);
+        public bool UpdateEntity(T entity);
+        public bool SaveAll();
+    }
+}
---
+++ Data/Repositories/Implementations/CRUDRepository.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Data.Repositories.Implementations
+{
+    public abstract class CRUDRepository<T> : ICRUDRepository<T> where T : class
+    {
+        private DutchTreatDbContext _dbContext;
+        private readonly ILogger<CRUDRepository<T>> _logger;
+
+        protected CRUDRepository(DutchTreatDbContext dbContext, ILogger<CRUDRepository<T>> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+        public bool AddEntity(T entity)
+        {
+            bool added = false;
+            try
+            {
+                _dbContext.Set<T>().Add(entity);
+                added = true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Entity was not added:\n{ex}");
+                
+            }
+            return added;
+        }
+
+        public bool DeleteEntity(T entity)
+        {
+            bool deleted = false;
+            try
+            {
+                _dbContext.Set<T>().Remove(entity);
+                deleted = true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Entity was not deleted:\n{ex}");
+
+            }
+            return deleted;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            var result = _dbContext.Set<T>();
+            try
+            {
+                if(result != null)
+                {
+                    return result;
+                }              
+            }
+            catch (Exception ex)
+            {
+                Type type = typeof(T);
+                _logger.LogError($"No entries for {type.Name} were found:\n{ex}");
+            }
+            return result;
+        }
+
+        public bool SaveAll()
+        {
+            return _dbContext.SaveChanges() > 0;
+        }
+
+        public bool UpdateEntity(T entity)
+        {
+            bool updated = false;
+            try
+            {
+                var entityToUpdate = _dbContext.Set<T>().FirstOrDefault(e => e.Equals(entity));
+                if (true)
+                {
+                    entityToUpdate = entity;
+                    updated = true;
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Entity was not updated:\n{ex}");
+
+            }
+            return updated;
+        }
+    }
+}
