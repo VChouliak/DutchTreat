958906cfe7f6ece9766a197915c1f892d7c60575
VChouliak <chouliak@hotmail.com>
2020-07-23 18:20:06 +0200

    Create OrderRepository

---
+++ Data/Repositories/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using DutchTreat.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Data.Repositories
+{
+    public interface IOrderRepository : ICRUDRepository<Order>
+    {
+      
+    }
+}
