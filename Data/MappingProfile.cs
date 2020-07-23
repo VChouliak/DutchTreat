3296712a8f08459aded78435d2f7d2706d352e6f
VChouliak <chouliak@hotmail.com>
2020-07-23 22:02:18 +0200

    Create MappingProfile

---
+++ Data/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DutchTreat.Data.Models;
+using DutchTreat.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Data
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrderViewModel>().ForMember(orderView => orderView.OrderId, ex=>ex.MapFrom(order=>order.Id)).ReverseMap();
+        }
+    }
+}
