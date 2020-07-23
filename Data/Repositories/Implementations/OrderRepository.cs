af97565b00d99f04e73a00cdf881c8137ad4281c
VChouliak <chouliak@hotmail.com>
2020-07-23 18:24:07 +0200

    Create OrderRepository

---
+++ Data/Repositories/Implementations/OrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using DutchTreat.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Data.Repositories.Implementations
+{
+    public class OrderRepository : CRUDRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(DutchTreatDbContext dbContext, ILogger<CRUDRepository<Order>> logger) : base(dbContext, logger)
+        {
+        }
+     
+    }
+}
