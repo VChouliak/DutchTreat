af8cc31478de03d3a5faa0bd8d7c96839133867a
VChouliak <chouliak@hotmail.com>
2020-07-24 22:15:15 +0200

    Create Helper

---
+++ Helpers/OrderItemHelper.cs
@@ -0,0 +1,42 @@
+﻿using DutchTreat.Data.Models;
+using DutchTreat.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Helpers
+{
+    public class OrderItemHelper
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IOrderItemRepository _orderItemRepository;
+
+        public OrderItemHelper()
+        {
+
+        }
+        public OrderItemHelper(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;           
+        }
+        public OrderItemHelper(IOrderRepository orderRepository, IOrderItemRepository orderItemRepository)
+        {
+            _orderRepository = orderRepository;
+            _orderItemRepository = orderItemRepository;
+        }
+
+        public bool IsOrderExists(int orderId)
+        {
+            return _orderRepository
+                .GetAllOrders()
+                .Where(order => order.Id == orderId)
+                .FirstOrDefault() != null;
+        }
+
+        public bool IsItemExists(int id)
+        {
+            return _orderItemRepository.FindBy(nameof(OrderItem.Id),id).FirstOrDefault() != null;
+        }
+    }
+}
