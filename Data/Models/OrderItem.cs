b64a8fedfa3d356d6e42496527960d46394f4e4f
VChouliak <chouliak@hotmail.com>
2020-07-24 22:19:11 +0200

    Update

--- Data/Models/OrderItem.cs
+++ Data/Models/OrderItem.cs
@@ -7,9 +7,11 @@ namespace DutchTreat.Data.Models
     {
         public int Id { get; set; }
         public Product Product { get; set; }
+        public int ProductId { get; set; }
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
         public Order Order { get; set; }
+        public int OrderId { get; set; }
 
         public override bool Equals(object obj)
         {
