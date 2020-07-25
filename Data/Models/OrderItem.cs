d6b37686a9018559f7635bf36e7ce6a331adb8cc
VChouliak <chouliak@hotmail.com>
2020-07-25 16:54:31 +0200

    Add Annotations

--- Data/Models/OrderItem.cs
+++ Data/Models/OrderItem.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DutchTreat.Data.Models
 {
     public class OrderItem
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public Product Product { get; set; }
+        [ForeignKey("Product")]
         public int ProductId { get; set; }
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
         public Order Order { get; set; }
+        [ForeignKey("Order")]
         public int OrderId { get; set; }
 
         public override bool Equals(object obj)
--- Data/Models/Product.cs
+++ Data/Models/Product.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,8 @@ namespace DutchTreat.Data.Models
 {
     public class Product
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Category { get; set; }
         public string Size { get; set; }
