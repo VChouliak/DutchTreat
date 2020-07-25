43b0fe2125cdd2ca356193512e9cda78d68eafa4
VChouliak <chouliak@hotmail.com>
2020-07-25 20:15:09 +0200

    Add Identity

--- Data/Migrations/20200725164710_AddIdentity.Designer.cs
+++ Data/Migrations/20200725164710_AddIdentity.Designer.cs
@@ -4,14 +4,16 @@ using DutchTreat.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DutchTreat.Migrations
 {
     [DbContext(typeof(DutchTreatDbContext))]
-    partial class DutchTreatDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200725164710_AddIdentity")]
+    partial class AddIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
