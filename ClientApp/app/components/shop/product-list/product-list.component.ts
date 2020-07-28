71ea411212d351a8393bd9858ad75bea9931f00a
VChouliak <chouliak@hotmail.com>
2020-07-28 13:39:44 +0200

    Implement onInit and add type for products array

--- ClientApp/app/components/shop/product-list/product-list.component.ts
+++ ClientApp/app/components/shop/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'ClientApp/app/services/data.service';
+import {Product} from '../../../models/product'
 
 @Component({
   selector: 'product-list',
@@ -9,12 +10,22 @@ import { DataService } from 'ClientApp/app/services/data.service';
 })
 export class ProductListComponent implements OnInit {
 
-  public products = [];
+  public products: Product[] = [];
   constructor(private data: DataService) { 
-    this.products = data.products;
+    
   }
 
   ngOnInit(): void {
+    this.data.loadProducts().subscribe(
+      success=>{
+        if(success){
+          this.products = this.data.products;
+        }
+      },
+      error => {
+        console.log(error)
+      }
+    );
   }
 
 }
