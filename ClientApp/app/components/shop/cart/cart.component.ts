239084f89de3df14743f9e7a88a64c89c4690fa4
VChouliak <chouliak@hotmail.com>
2020-07-29 12:31:44 +0200

    Implement onCheckout method

--- ClientApp/app/components/shop/cart/cart.component.ts
+++ ClientApp/app/components/shop/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'ClientApp/app/services/data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -9,8 +10,15 @@ import { DataService } from 'ClientApp/app/services/data.service';
 })
 export class CartComponent implements OnInit {
 
-  constructor(private data: DataService) { }
+  constructor(private data: DataService, private router: Router) { }
 
+  onCheckout(){
+    if(this.data.loginRequired){
+      this.router.navigate(["login"]);
+    }else{
+      this.router.navigate(["checkout"]);
+    }
+  }
   ngOnInit(): void {
   }
 
