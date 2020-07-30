a357f506ba0f3d9bf61eaf6ae8ead8056b3bb4c0
VChouliak <chouliak@hotmail.com>
2020-07-30 23:26:04 +0200

    Create cart.component.css

--- ClientApp/app/components/shop/cart/cart.component.ts
+++ ClientApp/app/components/shop/cart/cart.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-cart',
   templateUrl: "./cart.component.html",
-  styles: [
+  styleUrls: ["./cart.component.css"
   ]
 })
 export class CartComponent implements OnInit {
