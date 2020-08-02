fb87ed2c4d704bcd1a65f256e4a58c8647dcbc9c
VChouliak <chouliak@hotmail.com>
2020-08-02 18:47:55 +0200

    Implement deleteItem, OnInit, getDescription

--- ClientApp/app/components/shop/checkout/checkout.component.ts
+++ ClientApp/app/components/shop/checkout/checkout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from 'ClientApp/app/services/data.service';
+import { OrderItem } from 'ClientApp/app/models/order';
+import { newArray } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-checkout',
@@ -10,10 +12,14 @@ import { DataService } from 'ClientApp/app/services/data.service';
 })
 export class CheckoutComponent implements OnInit {
 
+  
   constructor(public data: DataService, public router: Router) { }
-
+  public items: OrderItem[] = new Array<OrderItem>();
   errorMessage: string = "";
 
+  deleteItem(id: number){
+    this.data.deleteFromOrder(id);
+  }
   onCheckout() {
     this.data.checkout().subscribe(success=>{
       if(success){
@@ -23,7 +29,12 @@ export class CheckoutComponent implements OnInit {
     err=> this.errorMessage = "Failed to save order");
   }
 
+  getDescription(id: number): string{
+    const description = this.data.products.find(p=>p.id==id).artDescription;
+    return description;
+  }
   ngOnInit(): void {
+    this.items = this.data.order.items;
   }
 
 }
