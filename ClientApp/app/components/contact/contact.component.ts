303c1f82b30bd41edd9fc7f93aafd4ac6f7cf6f7
VChouliak <chouliak@hotmail.com>
2020-08-03 17:17:20 +0200

    Create contact component

---
+++ ClientApp/app/components/contact/contact.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'ClientApp/app/services/data.service';
+import { Contact } from 'ClientApp/app/models/contact';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.css"
+  ]
+})
+export class ContactComponent implements OnInit {
+
+  public errorMessage: string = "";
+  public successMessage: string="";
+  public contactMessage: Contact = new Contact();
+
+  // public messageRequest={
+  //     username:"",
+  //     email:"",
+  //     subject:"",
+  //     message:""  
+  // };
+
+  constructor(private data: DataService) { }
+
+  ngOnInit(): void {
+  }
+
+  sendMessageAndGetResponse(){
+    this.data.sendMessage(this.contactMessage).subscribe(
+    success=>{
+      if(success){
+        this.successMessage ="Your message was delivered!!!"
+      }else{
+        this.successMessage ="Message delivery failed."
+      }
+    });
+  }
+
+
+}
